761ddb9f6e8e90bb0946d80e6a76beefa8834ddf
beelmaryama-netizen <beelmaryama@gmail.com>
2026-06-17 11:20:33 -0400

    Ajout du projet complet

--- server/routes/livresRoutes.js
+++ server/routes/livresRoutes.js
@@ -6,8 +6,39 @@ const {
     getLivresEmpruntesByEmail
 } = require("../controllers/livresController");
 
+/**
+ * @swagger
+ * /api/livres:
+ *   get:
+ *     summary: Récupérer la liste des livres disponibles
+ *     tags:
+ *       - Livres
+ *     responses:
+ *       200:
+ *         description: Liste des livres récupérée avec succès
+ */
+
 // 📚 livres disponibles
 router.get("/", getLivres);
+
+/**
+ * @swagger
+ * /api/livres/emprunts:
+ *   get:
+ *     summary: Récupérer les livres empruntés par un utilisateur
+ *     tags:
+ *       - Emprunts
+ *     parameters:
+ *       - in: query
+ *         name: email
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Adresse courriel de l'utilisateur
+ *     responses:
+ *       200:
+ *         description: Liste des emprunts récupérée avec succès
+ */
 
 // 👤 emprunts par email
 router.get("/emprunts", getLivresEmpruntesByEmail);
--- server/server.js
+++ server/server.js
@@ -3,15 +3,50 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+// Swagger
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 const livresRoutes = require("./routes/livresRoutes");
 
+// Configuration Swagger
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Bibliothèque",
+      version: "1.0.0",
+      description: "Documentation de l'API de la bibliothèque numérique",
+    },
+    servers: [
+      {
+        url: "https://maryama-belouaar-evaluation-finaleh26.onrender.com",
+      },
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+// Génération de la documentation Swagger
+const specs = swaggerJsdoc(options);
+
+// Route Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+// Route API livres
 app.use("/api/livres", livresRoutes);
 
+// Démarrage du serveur
 if (process.env.NODE_ENV !== "test") {
   app.listen(process.env.PORT, () => {
     console.log(`Serveur lancé sur le port ${process.env.PORT}`);
