d208ce5dc9ff5fce28eaed25aed66831f4d3fc89
beelmaryama-netizen <beelmaryama@gmail.com>
2026-06-17 11:40:44 -0400

    Ajout Swagger

--- server/server.js
+++ server/server.js
@@ -2,27 +2,33 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
 const swaggerUi = require("swagger-ui-express");
 const specs = require("./config/swagger");
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Swagger
+// Swagger documentation
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-// Routes
+// Routes API
 const livresRoutes = require("./routes/livresRoutes");
-
 app.use("/api/livres", livresRoutes);
 
-// Démarrage du serveur
+// Route test
+app.get("/", (req, res) => {
+  res.send("API Bibliothèque fonctionne correctement");
+});
+
+// Lancement du serveur
 if (process.env.NODE_ENV !== "test") {
-  app.listen(process.env.PORT, () => {
-    console.log(`Serveur lancé sur le port ${process.env.PORT}`);
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Serveur lancé sur le port ${PORT}`);
   });
 }
 
