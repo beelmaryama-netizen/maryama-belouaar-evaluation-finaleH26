93579ba7fc0eacfbf96bf06a11c7e40d51d0c62a
beelmaryama-netizen <beelmaryama@gmail.com>
2026-06-17 10:10:52 -0400

    Mise à jour du projet

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
-    if(err){
-        console.log(err);
-    } else {
-        console.log("MySQL connecté");
-    }
+  if (err) {
+    console.error("Erreur connexion MySQL :", err);
+  } else {
+    console.log("MySQL connecté");
+  }
 });
 
 module.exports = db;
